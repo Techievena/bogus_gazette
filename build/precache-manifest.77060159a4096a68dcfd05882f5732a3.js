2071fec28bb70ca0a78cb171720e02a44470f60f
Techievena <abinashsena@gmail.com>
2020-10-19 11:12:04 +0530

    [Vulnerabilities] Update dependencies

---
+++ build/precache-manifest.77060159a4096a68dcfd05882f5732a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e0bb5f922dda30c056",
+    "url": "/static/css/main.ab3bd387.chunk.css"
+  },
+  {
+    "revision": "44e0bb5f922dda30c056",
+    "url": "/static/js/main.44e0bb5f.chunk.js"
+  },
+  {
+    "revision": "38c533ccc4b087fbc973",
+    "url": "/static/css/1.5c7b40c3.chunk.css"
+  },
+  {
+    "revision": "38c533ccc4b087fbc973",
+    "url": "/static/js/1.38c533cc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3eada0530d70b562c6f8ad8d3236afb3",
+    "url": "/index.html"
+  }
+];
